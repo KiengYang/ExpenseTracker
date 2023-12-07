8305f080340568ec6a0d35c783657f75aa0d5921
KiengYang <151807188+KiengYang@users.noreply.github.com>
2023-12-07 08:42:36 +0700

    Add files via upload

--- Login.java
+++ Login.java
@@ -3,7 +3,11 @@ package ExpenseTracker.User;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class Login {
@@ -51,11 +55,79 @@ public class Login {
         exit = true;
       }
     }
-    if (exit == true) {
+    if (exit) {
       System.out.println("Login complete");
       System.out.println(" ");
+
+      handleExpenses(loginUser);
     } else {
-      System.out.println("User doesnt exist");
+      System.out.println("User doesn't exist");
     }
+  }
+
+  private static void handleExpenses(User user) {
+    Scanner sc = new Scanner(System.in);
+    int choice;
+
+    do {
+      System.out.println("1. View Expenses");
+      System.out.println("2. Add Expense");
+      System.out.println("3. Logout");
+
+      choice = sc.nextInt();
+      sc.nextLine(); // Consume newline character
+
+      switch (choice) {
+      case 1:
+        viewExpenses(user);
+        break;
+      case 2:
+        addExpense(user, sc);
+        break;
+      case 3:
+        System.out.println("Logged out");
+        break;
+      default:
+        System.out.println("Invalid choice");
+        break;
+      }
+    } while (choice != 3);
+
+    sc.close();
+  }
+
+  private static void viewExpenses(User user) {
+    List<Expense> expenses = user.getExpenses();
+    System.out.println("Expenses for " + user.getUsername() + ":");
+    for (Expense expense : expenses) {
+      System.out.println(expense.getDate() + " - $" + expense.getAmount() +
+                         " - " + expense.getDescription());
+    }
+  }
+
+  private static void addExpense(User user, Scanner sc) {
+    System.out.println("Enter amount:");
+    double amount = sc.nextDouble();
+    sc.nextLine(); // Consume newline character
+
+    System.out.println("Enter description:");
+    String description = sc.nextLine();
+
+    System.out.println(
+        "Enter date (MM/dd/yyyy):"); // Adjust date format as needed
+    String dateString = sc.nextLine();
+
+    Date date;
+    try {
+      SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+      date = dateFormat.parse(dateString);
+    } catch (ParseException e) {
+      System.out.println(
+          "Invalid date format. Please enter date in MM/dd/yyyy format.");
+      return;
+    }
+
+    user.addExpense(amount, date, description);
+    System.out.println("Expense added successfully!");
   }
 }
--- User.java
+++ User.java
@@ -1,20 +1,31 @@
 package ExpenseTracker.User;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class User {
   private String username;
   private String password;
+  private List<Expense> expenses;
 
-  public User(String username, String password) {
+  public User(String username, String password ) {
     this.username = username;
     this.password = password;
+    this.expenses = new ArrayList<>();
   }
 
   public String getUsername() { return username; }
 
   public String getPassword() { return password; }
+
+  public List<Expense> getExpenses() { return expenses; }
+
+  public void addExpense(double amount, Date date, String description) {
+    Expense expense = new Expense(amount, date, description);
+    expenses.add(expense);
+  }
 
   public String toString() {
     return "Username: " + username + ", Password: " + password;
