519dc399d5ec7d74414efd6c3f3571a85e63c5cb
KiengYang <151807188+KiengYang@users.noreply.github.com>
2024-01-03 22:08:47 +0700

    Delete Admin.java

--- Admin.java
+++
@@ -1,9 +0,0 @@
-package ExpenseTracker.User;
-
-public class Admin extends Person {
-  public Admin(String username, String password) { super(username, password); }
-
-  public void performAdminTask() {
-    // Logic for performing administrative tasks
-  }
-}
